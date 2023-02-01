1956b83570d6a8641061e2c4a44f20dc2b87f9b5
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-02-01 16:37:09 -0300

    completed uploading area

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -1,6 +1,7 @@
 import { Photo } from "../types/Photo";
 import { storage } from "../libs/firebase";
-import { ref, listAll, getDownloadURL } from 'firebase/storage'
+import { ref, listAll, getDownloadURL, uploadBytes } from "firebase/storage";
+import { v4 as createId } from "uuid";
 
 export const getAll = async () => {
   let list: Photo[] = [];
@@ -8,14 +9,33 @@ export const getAll = async () => {
   const imagesFolder = ref(storage, "images");
   const photoList = await listAll(imagesFolder);
 
-  for(let i in photoList.items){
+  for (let i in photoList.items) {
     let photoUrl = await getDownloadURL(photoList.items[i]);
 
     list.push({
       name: photoList.items[i].name,
-      url: photoUrl
+      url: photoUrl,
     });
   }
 
   return list;
+};
+
+export const insert = async (file: File) => {
+  if (["image/jpeg", "image/jpg", "image/png"].includes(file.type)) {
+    
+    let randomName = createId();
+    let newFile = ref(storage, `images/${randomName}`);
+
+    let upload = await uploadBytes(newFile, file);
+    let photoUrl = await getDownloadURL(upload.ref);
+    
+    return {
+      name: upload.ref.name,
+      url: photoUrl
+    } as Photo;
+
+  } else {
+    return new Error("Tipo de arquivo não permitido.");
+  }
 };
