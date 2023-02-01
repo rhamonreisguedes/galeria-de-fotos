9a1a28e9fec628ab6e156d2fc5c223c59d77d873
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-02-01 15:46:58 -0300

    set up of initial page and upload of photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from "./App.styles";
+import * as Photos from './services/photos';
+import {useState, useEffect} from 'react';
+import {Photo} from './types/Photo'
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([])
+
+  useEffect(() =>{
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+
+    getPhotos();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/* Área de upload */}
+        
+        {loading && 
+          <C.ScreenWarning>
+            <div className="emoji">✋</div>
+            <div>Carregando...</div>  
+          </C.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 && 
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <div>{item.name}</div>
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loading && photos.length === 0 && 
+          <C.ScreenWarning>
+          <div className="emoji">😢</div>
+          <div>Não há fotos cadastradas.</div>  
+        </C.ScreenWarning>
+        }
+      </C.Area>
+    </C.Container>
   );
-}
+};
 
 export default App;
